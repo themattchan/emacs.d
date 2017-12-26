c03cc0c04db6b38ac357b73e587838f71cafa8b2
Matthew Chan <matt@awakenetworks.com>
2017-12-25 20:34:08 -0800

    file header

--- lisp-matt/matt-prog-haskell.el
+++ lisp-matt/matt-prog-haskell.el
@@ -1,7 +1,27 @@
-;;  -*- lexical-binding: t -*-
-;; ----------------------------------------------------------------------
-;; Haskell
-;; ----------------------------------------------------------------------
+;;; matt-prog-haskell.el --- Haskell mode settings -*- lexical-binding: t -*-
+
+;;; Copyright (c) 2016-2018 Matthew Chan
+;;; Author: Matthew Chan <matt@parametri.city>
+;;; URL: http://github.com/themattchan/emacs.d
+;;; Package-Requires: ((s "1.12.0") (dash "2.13.0") (dash-functional "1.2.0") (emacs "24"))
+
+;;; License:
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
 
 (use-package haskell-mode :ensure t)
 (require 'ghc-nix)
